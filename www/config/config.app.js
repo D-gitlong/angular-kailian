910bc967be2556e3580c864f4f9e780e4135f6be
D-gitlong <chulong.0223@qq.com>
2018-05-07 18:14:13 +0800

    打包文件

---
+++ www/config/config.app.js
@@ -0,0 +1,13 @@
+var GLOBAL_CONFIG = {
+	// 'API_HOST': 'http://api-v2.91go.com',
+	'API_HOST': 'http://api-dev-v2.91go.com',
+	// 'API_HOST': 'http://api-test-v2.91go.com',
+	'APP_NAME': '凯联健康',
+	'APP_DESC': '凯联健康',
+	'APP_KEYWORDS': '凯联健康',
+	'DEBUG': false,
+	'ENCRYPTED':false,
+	 //如果公众号没有配置，将for_weixin置为false，才能在微信中使用
+	'FOR_WEIXIN':false,
+	'VERSION': '版本号 2.0.09'
+};
